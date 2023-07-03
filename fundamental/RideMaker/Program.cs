9a3b510b658ac80e90ebf5ba6db9cdd3a040b14b
ken594 <weihui99525@gmail.com>
2023-07-03 15:21:18 -0700

    Rider Maker (practice creating classes)

---
+++ fundamental/RideMaker/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Vehicle a = new Vehicle("Tigger", "Black and White");
+Vehicle b = new Vehicle("Ocean", 2, "blue", true);
+Vehicle c = new Vehicle("Batman", "Black");
+Vehicle d = new Vehicle("Rocket", 1, "Red", true);
+
+// MyVehicle.ShowInfo();
+// Console.WriteLine("-------------------");
+// MyVehicle.Travel(5000);
+
+// NewVehicle.ShowInfo();
+
+List<Vehicle> vehicleList = new List<Vehicle> { a, b, c, d };
+foreach (Vehicle i in vehicleList)
+{
+    i.ShowInfo();
+    Console.WriteLine("-------------------");
+}
+
+d.Travel(100);
+Console.WriteLine("-------------------");
+d.ShowInfo();
+Console.WriteLine(d._distanceTraveled);
---
+++ fundamental/RideMaker/Vehicle.cs
@@ -0,0 +1,46 @@
+class Vehicle
+{
+    public string Name;
+    public string color;
+    int numPassengers;
+    bool hasEngine;
+    double distanceTraveled;
+
+    public double _distanceTraveled
+    {
+        get { return distanceTraveled; }
+    }
+
+    public Vehicle(string n, int num, string col, bool e)
+    {
+        Name = n;
+        numPassengers = num;
+        color = col;
+        hasEngine = e;
+    }
+
+    public Vehicle(string n, string col)
+    {
+        Name = n;
+        color = col;
+        numPassengers = 4;
+        hasEngine = true;
+        distanceTraveled = 0;
+    }
+
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Number of Passengers: {numPassengers}");
+        Console.WriteLine($"Color: {color}");
+        Console.WriteLine($"HasEngine: {hasEngine}");
+        Console.WriteLine($"distance Traveled: {distanceTraveled}");
+    }
+
+    public void Travel(int distance)
+    {
+        distanceTraveled += distance;
+        Console.WriteLine($"{Name} has gone: {distanceTraveled} miles");
+    }
+}
