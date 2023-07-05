8bbdf8cb93970040aa1976c7d792fabb9f8ede75
ken594 <weihui99525@gmail.com>
2023-07-04 19:23:05 -0700

    Razor Fun

---
+++ ASP-MVC/RazorFun/Controllers/IndexController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun.Controllers;
+
+public class IndexController : Controller
+{
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+        return View();
+    }
+}
