17310b337f0652db38e7be0293e4bf84ffc4c2ef
ken594 <weihui99525@gmail.com>
2023-07-09 11:19:50 -0700

    First LINQ Practice

---
+++ ORMs/LINQPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Product[] myProducts = new Product[]
+{
+    new Product { Name = "Jeans", Category = "Clothing", Price = 24.7 },
+    new Product { Name = "Socks", Category = "Clothing", Price = 8.12 },
+    new Product { Name = "Scooter", Category = "Vehicle", Price = 99.99 },
+    new Product { Name = "Skateboard", Category = "Vehicle", Price = 24.99 },
+    new Product { Name = "Skirt", Category = "Clothing", Price = 17.5 }
+};
+
+// sort by price
+IEnumerable<Product> sortProductsByPrice = myProducts.OrderByDescending(prod => prod.Price);
